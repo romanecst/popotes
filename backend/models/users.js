0e8cd994ded5cf04389d62adff2c422d9b80e45b
Nicolas Baudry <baudrynicolas3@orange.fr>
2020-12-07 10:53:10 +0100

    initialisation bdd creation dossier modele

---
+++ backend/models/users.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+    token: String,
+    salt: String, 
+})
+
+const userModel = mongoose.model('users', userSchema)
+
+module.exports = userModel
