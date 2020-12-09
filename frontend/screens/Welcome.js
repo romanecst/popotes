cb2c1b7041989ad3d6a5348c63e1da2cf288e2e0
Remi Ringard <ringard_remi@yahoo.fr>
2020-12-09 09:47:14 +0100

    b minuscule

--- frontend/screens/Welcome.js
+++ frontend/screens/Welcome.js
@@ -49,7 +49,7 @@ export default function welcome({navigation}) {
     };
 
     return (
-        <ImageBackground source={require('../assets/Background.jpeg')} style={{ flex: 1 }}>
+        <ImageBackground source={require('../assets/background.jpeg')} style={{ flex: 1 }}>
             <View style={styles.container}>
                 <Image style={{ width: 300, height: 300 }} source={require('../assets/logo.png')} />
                 <Text h1 style={{ marginTop: 120, color: '#FFFF', fontFamily: 'Kohinoor Telugu' }}>Welcome ! </Text>
