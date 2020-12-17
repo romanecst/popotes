f0b5bd4b44693b8848372101d84ae4b18f0b3a98
Remi Ringard <ringard_remi@yahoo.fr>
2020-12-17 11:54:27 +0100

    modif List

--- frontend/screens/List.js
+++ frontend/screens/List.js
@@ -165,19 +165,19 @@ function List(props) {
 
   if (lists.length == 0) {
     var listsShop =
-    <View>
-     <Text style={{fontFamily: 'Kohinoor Telugu', fontSize:20, marginTop:40, color:'grey', marginLeft:78}}>No shopping list</Text>
-     <Image
-    style={{width:150, height:150, marginLeft:78}}
-    source={require('../assets/sad.png')}/>
-    </View>
-    
-} else {
+      <View>
+        <Text style={{ fontFamily: 'Kohinoor Telugu', fontSize: 20, marginTop: 40, color: 'grey', marginLeft: 78 }}>No shopping list</Text>
+        <Image
+          style={{ width: 150, height: 150, marginLeft: 78 }}
+          source={require('../assets/sad.png')} />
+      </View>
+
+  } else {
     var listsShop = lists.map((el, i) => (
-      <View style={{ alignItems: 'center', justifyContent: 'center', backgroundColor: 'white', borderRadius: 30 , marginBottom:12}}>
+      <View style={{ alignItems: 'center', justifyContent: 'center', backgroundColor: 'white', borderRadius: 30, marginBottom: 12 }}>
         <TouchableOpacity
           onPress={() => { props.currentList(el); props.addingredientList(el.ingredients); props.navigation.navigate('GlobalList') }}>
-          <ListItem key={i} containerStyle={{ width: 280, flexDirection: 'row', alignItems: 'center', borderRadius:30 }}>
+          <ListItem key={i} containerStyle={{ width: 280, flexDirection: 'row', alignItems: 'center', borderRadius: 30 }}>
             <ListItem.Content style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', }}>
               <Text> {el.name}</Text>
               <View>
@@ -188,8 +188,8 @@ function List(props) {
         </TouchableOpacity>
       </View>
     ))
-    
-};  
+
+  };
 
 
   return (
@@ -198,7 +198,7 @@ function List(props) {
       <Header
         containerStyle={{ backgroundColor: '#febf63', height: 90, paddingTop: 50 }}
         leftComponent={<AntDesign name="leftcircleo" size={24} color="white" onPress={() => { props.navigation.goBack(null) }} />}
-        centerComponent={{ text: 'LIST', style: { color: '#fff', fontFamily: 'Kohinoor Telugu', fontSize:22 } }}
+        centerComponent={{ text: 'LIST', style: { color: '#fff', fontFamily: 'Kohinoor Telugu', fontSize: 22 } }}
         rightComponent={<Fontisto name="shopping-basket" size={24} color="white" onPress={() => { props.navigation.navigate('List') }} />}
       />
 
@@ -208,30 +208,35 @@ function List(props) {
 
 
         {/* ---------------------------LISTE EXISTANTE FAVORITE------------------------------------------  */}
-        <ScrollView style={{ marginTop: 10, backgroundColor: "#FFF2DF", width: 300, height: 300, borderRadius: 20}}>
-          { listsShop  }
+        <ScrollView style={{ marginTop: 10, backgroundColor: "#FFF2DF", width: 300, height: 300, borderRadius: 20 }}>
+          {listsShop}
         </ScrollView>
 
         {/* ------------------------------ OVERLAY -----------------------------------------------*/}
         <Overlay overlayStyle={{ backgroundColor: '#dfe6e9', borderRadius: 50, }} isVisible={visible} onBackdropPress={toggleOverlay} >
           <View style={styles.overlay}>
             <Text style={{ fontFamily: 'Kohinoor Telugu', fontSize: 18, paddingBottom: 30 }}>First, give a name to your list : </Text>
-            <Input placeholder='Name'
+            <Input
+              placeholder='Name of group'
               onChangeText={(value) => setText(value)}
-              value={text} />
+              value={text}
+              style={{backgroundColor:'white', borderRadius:10, paddingLeft:12}}
+              inputContainerStyle={{ borderBottomWidth: 0 }}
+              />
+
           </View>
+
           <Button
             title="Confirm"
+            buttonStyle={{ borderColor: 'white', marginTop: 10, padding: 10, marginHorizontal: 100, borderRadius: 30, marginBottom: 20, backgroundColor: "#febf63" }}
+            titleStyle={{ color: 'white', fontFamily: 'Kohinoor Telugu' }}
             onPress={() => { addList(); toggleOverlay() }}
-            type="clear"
-            buttonStyle={{ borderColor: 'white', justifyContent: 'center' }}
-            titleStyle={{ color: 'black', fontFamily: 'Kohinoor Telugu', fontSize: 18, paddingTop: 30 }}
-
           />
+
         </Overlay>
 
 
-        <Text style={{ fontSize: 22, marginTop: 40, marginBottom: 20, fontFamily: 'Kohinoor Telugu' }}>Create a new list</Text>
+        <Text style={{ fontSize: 22, marginTop: 40, marginBottom: 20, fontFamily: 'Kohinoor Telugu', }}>Create a new list</Text>
 
         {/* --------------BOUTON CREATION D'UNE LISTE DE FAVORI -----------------------------------------------*/}
         <Ionicons name="ios-add-circle-outline" size={60} color="black" onPress={toggleOverlay} />
